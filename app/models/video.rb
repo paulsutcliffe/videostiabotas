d59b069173a1306106c025ab6fa4c51d4b9d7bad
Paul Sutcliffe <paul@kosmyka.com>
2012-08-29 12:46:11 -0500

    Implemented Amazon S3

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,5 +1,8 @@
 class Video < ActiveRecord::Base
   belongs_to :category
   belongs_to :character
-  has_attached_file :movie
+  has_attached_file :movie,
+                    :storage => :s3,
+                    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                    :path => "movies/:id/:filename"
 end
